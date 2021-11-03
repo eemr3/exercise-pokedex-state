7690590f47c5b33d05d847a2da0e88cb478c63db
Emerson <eemr3@yahoo.com.br>
2021-11-02 23:00:00 -0300

    build(Pokedex): Implementa funcionalidade do requisito 2

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -15,19 +15,29 @@ class Pokedex extends Component {
     };
     this.handleNextPokemon = this.handleNextPokemon.bind(this);
     this.getPokemon = this.getPokemon.bind(this);
+    this.handleFilterPokemon = this.handleFilterPokemon.bind(this);
   }
 
   getPokemon() {
-    return pokemons.filter((pokemon) => pokemon);
+    return pokemons.filter((pokemon) => pokemon.type === this.state.typePokemons);
   }
 
   handleNextPokemon(pokemonLength) {
     if (this.state.pokemonIndex === pokemonLength - 1) {
       this.setState({ pokemonIndex: 0 });
+    } else {
+      this.setState((prevState) => ({
+        pokemonIndex: prevState.pokemonIndex + 1,
+      }));
     }
-    this.setState((prevState) => ({
-      pokemonIndex: prevState.pokemonIndex + 1,
-    }));
+  }
+
+  handleFilterPokemon(typePokemon) {
+    this.setState({
+      typePokemons: typePokemon,
+      pokemonIndex: 0,
+    });
+    console.log(this.state.typePokemons);
   }
 
   render() {
@@ -38,6 +48,10 @@ class Pokedex extends Component {
         <Header />
         <main className="container">
           <Pokemon pokemons={pokemon} key={pokemons.id} />
+          <div className="container-button">
+            <Button onClick={() => this.handleFilterPokemon('Fire')}>Fire</Button>
+            <Button onClick={() => this.handleFilterPokemon('Psychic')}>Psychic</Button>
+          </div>
           <Button onClick={() => this.handleNextPokemon(allPokemon.length)}>
             Próximo
           </Button>
