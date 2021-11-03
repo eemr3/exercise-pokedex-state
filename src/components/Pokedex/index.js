8ea0684ffa661ab1275acf414715124ad32bc732
Emerson <eemr3@yahoo.com.br>
2021-11-02 22:33:24 -0300

    Ajustes no CSS dos componetes Pokedex e Pokemon

--- src/components/Pokedex/index.js
+++ src/components/Pokedex/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import pokemons from '../../services/data';
+import Button from '../Button';
 import Header from '../Header';
 import Pokemon from '../Pokemon';
 
@@ -11,13 +12,23 @@ class Pokedex extends Component {
     this.state = {
       pokemonIndex: 0,
     };
+    this.handleNextPokemon = this.handleNextPokemon.bind(this);
   }
+
+  handleNextPokemon() {
+    if (this.state.pokemonIndex === pokemons.length - 1) {
+      this.setState({ pokemonIndex: 0 });
+    }
+    this.setState((prevState) => ({ pokemonIndex: prevState.pokemonIndex + 1 }));
+  }
+
   render() {
     return (
       <>
         <Header />
         <main className="container">
           <Pokemon pokemons={pokemons[this.state.pokemonIndex]} key={pokemons.id} />
+          <Button onClick={this.handleNextPokemon}>Próximo</Button>
         </main>
       </>
     );
